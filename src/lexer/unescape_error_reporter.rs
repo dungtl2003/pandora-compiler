e2d716f4f5e7c36f86600081673b5c1810543b44
Tran Luu Dung <63690773+dungtl2003@users.noreply.github.com>
2024-11-05 19:19:59 +0700

    feat: add error reporter and dummy test error report files

---
+++ src/lexer/unescape_error_reporter.rs
@@ -0,0 +1,108 @@
+use crate::error_handler::{ErrorEmitter, ERROR_CODE_URL};
+use miette::{Diagnostic, SourceSpan};
+use thiserror::Error;
+
+// TODO: Don't know where to write block comment and unknown.
+#[derive(Error, Debug, Diagnostic)]
+#[error("{msg}")]
+#[diagnostic(
+    code(E0758),
+    url("{}#{}", ERROR_CODE_URL, self.code().unwrap())
+)]
+struct UnterminatedBlockComment {
+    msg: String,
+    #[label("...as last nested comment started here, maybe you want to close this instead?")]
+    last_start_span: SourceSpan,
+    #[label("...and last nested comment terminates here.")]
+    last_terminated_span: SourceSpan,
+}
+
+#[derive(Error, Debug, Diagnostic)]
+#[error("unknown symbol '{sym}'")]
+struct UnknownSymbol {
+    sym: String,
+    #[label]
+    span: SourceSpan,
+}
+
+#[derive(Error, Debug, Diagnostic)]
+#[error("unknown character escape: `{bad_char}`")]
+#[diagnostic(
+    help("if you meant to write a literal backslash (perhaps escaping in a regular expression), consider a raw string literal"),
+)]
+struct UnknownCharEscape {
+    #[label("unknown character escape")]
+    bad_char_span: SourceSpan,
+
+    bad_char: char,
+}
+
+#[derive(Error, Debug, Diagnostic)]
+#[error("character constant must be escaped: `{escape_char}`")]
+struct EscapeOnlyChar {
+    escape_char: char,
+    escape_char_span: SourceSpan,
+}
+
+#[derive(Error, Debug, Diagnostic)]
+#[error("character literal may only contain one codepoint")]
+#[diagnostic(help("if you meant to write a string literal, use double quotes"))]
+struct MoreThanOneCharLiteral {
+    #[label]
+    span: SourceSpan,
+}
+
+#[derive(Error, Debug, Diagnostic)]
+#[error("empty character literal")]
+struct EmptyCharLiteral {
+    #[label("empty character literal")]
+    expected_char_span: SourceSpan,
+}
+
+impl ErrorEmitter {
+    pub fn report_unknown_symbol(&self, sym: String, span: SourceSpan) {
+        self.emit_err(UnknownSymbol { span, sym });
+    }
+
+    pub fn report_unterminated_block_comment(
+        &self,
+        msg: String,
+        last_start_span: SourceSpan,
+        last_terminated_span: SourceSpan,
+    ) {
+        self.emit_err(UnterminatedBlockComment {
+            msg,
+            last_terminated_span,
+            last_start_span,
+        });
+    }
+
+    pub fn report_empty_char_literal(&self, expected_char_span: SourceSpan) {
+        self.emit_err(EmptyCharLiteral { expected_char_span });
+    }
+
+    pub fn report_more_than_one_char_literal(&self, span: SourceSpan) {
+        self.emit_err(MoreThanOneCharLiteral { span });
+    }
+
+    pub fn report_escape_only_char_literal(&self, escape_char: char, escape_char_span: SourceSpan) {
+        self.emit_err(EscapeOnlyChar {
+            escape_char,
+            escape_char_span,
+        });
+    }
+
+    pub fn report_unknown_char_escape(&self, bad_char: char, bad_char_span: SourceSpan) {
+        self.emit_err(UnknownCharEscape {
+            bad_char,
+            bad_char_span,
+        });
+    }
+
+    pub fn report_escape_only_char(&self, escape_char: char, escape_char_span: SourceSpan) {
+        self.emit_err(EscapeOnlyChar {
+            escape_char,
+            escape_char_span,
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,40 @@ mod interner;
 #[path = "keyword.rs"]
 mod kw;
 mod lexer;
-mod parser;
+mod parse;
 mod session_global;
 mod visitor;
 
+use crate::error_handler::*;
+use miette::NamedSource;
 use std::fs;
+use std::sync::Arc;
 
 fn main() {
-    let data = fs::read_to_string("src/dummy_code.box").expect("unable to read file");
-    let tokens = lexer::tokenize(&data);
+    let file_path = "src/trash/".to_string();
+    let file_names = vec![
+        "unterminated_block_comment.box",
+        "number_literal_error.box",
+        "unterminated_raw_str.box",
+        "too_many_hashes_raw_str.box",
+        "unterminated_char.box",
+        "unescape_error.box",
+    ];
 
-    for token in tokens.iter() {
-        println!("{token:?}");
+    for file_name in file_names {
+        println!("================ BEGIN ================");
+        let data = Arc::new(
+            fs::read_to_string(file_path.clone() + file_name).expect("unable to read file"),
+        );
+        let source = NamedSource::new(file_name, Arc::clone(&data));
+        let emitter = ErrorEmitter {
+            file: Arc::new(source),
+        };
+        let tokens = parse::lexer::tokenize(&data, emitter);
+
+        for token in tokens.iter() {
+            println!("{token:?}");
+        }
+        println!("================ END ================");
     }
 }
